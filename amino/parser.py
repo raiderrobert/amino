ac13298e13c19da76b7ca96b10f3425c56904de5
rroskam <raiderrobert@gmail.com>
2022-03-02 09:47:24 -0500

    first pass

--- amino/parser.py
+++ amino/parser.py
@@ -1,3 +1,92 @@
+import dataclasses
+import enum
+import regex
+
+
+whitespace = regex.compile("[\s]+")
+word = regex.compile("[\w]+")
+colon = regex.compile(":")
+
+
+class TokenType(enum.Enum):
+    whitespace = 1
+    word = 2
+    colon = 3
+
+
+token_map = {
+    whitespace: TokenType.whitespace,
+    word: TokenType.word,
+    colon: TokenType.colon,
+}
+
+
+@dataclasses.dataclass
+class Token:
+    value: str
+    token_type: TokenType
+
+
+def _tokenizer(target: str) -> list[str]:
+    tokens: list = []
+    pos = 0
+    while len(target) > 0:
+        for pattern, token_type in token_map.items():
+            match_obj = pattern.match(target)
+            if match_obj is not None:
+                value = match_obj.group(0)
+                if token_type is not token_type.whitespace:
+                    tokens.append(Token(value, token_type))
+                pos = match_obj.end()
+                target = target[pos:]
+                break
+        else:
+            raise Exception(f'Unexpected characters at {target[pos:]}')
+    return tokens
+
+
+def load_schema():
+    schema: str = """
+    foo: int
+    """
+
+    return parse_schema(schema)
+
+
+class SchemaType(enum.Enum):
+    str = 'str'
+    int = 'int'
+
+
+def parse_types(_type):
+    match _type:
+        case "str":
+            return SchemaType.str
+        case "int":
+            return SchemaType.int
+
+    raise Exception(f'Unexpected type {_type}')
+
+
+@dataclasses.dataclass
+class Names:
+    name: str
+    _type: SchemaType
+
+
+def parse_schema(schema):
+    tokens = _tokenizer(schema)
+    parsed_schema = []
+    while len(tokens) > 0:
+        match tokens:
+            case [Token(name, TokenType.word), Token(_, TokenType.colon), Token(_type, TokenType.word), *remainder]:
+                parsed_schema.append(
+                    Names(name, parse_types(_type))
+                )
+                tokens = remainder
+
+    return parsed_schema
+
 
 def parse():
     pass
