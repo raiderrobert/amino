756adeb51d19a7ab6d4bff463c0c66ff8384790e
rroskam <raiderrobert@gmail.com>
2025-02-21 22:16:45 -0500

    more rewriting

--- amino/__init__.py
+++ amino/__init__.py
@@ -1,2 +1,8 @@
-from .schema_parser import load_schema  # noqa: F401
-from .compiler import compile  # noqa: F401
+"""
+Amino - A toolkit and DSL for custom rules engines.
+"""
+
+from amino.core import load_schema, Amino, CompiledRules
+
+__version__ = "0.1.0"
+__all__ = ["load_schema", "Amino", "CompiledRules"]
